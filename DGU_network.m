14d6ff023ea5765995b9d0eab25e1ba623a4ed88
Nicolashoi <nhoischen@student.ethz.ch>
2021-11-26 17:03:05 +0100

    implementation of DGU class and read parameters of network from txt file

--- DGU_network.m
+++ DGU_network.m
@@ -22,11 +22,16 @@ classdef DGU_network
        Agraph
        graph
        Rij_mat
+       L_tilde
        Iti_ref
        di_ref
        nb_subsystems
        ni = 2 % size of subsystem state
        %% Constraints
+       Vmax
+       Vmin
+       Imax
+       Imin
        Gx_i
        Gu_i
        fx_i
@@ -49,28 +54,37 @@ classdef DGU_network
                 obj.nb_subsystems = nb_subsystems;
                 obj.Rij_mat = Rij_mat;
                 obj.Agraph = Rij_mat;
-                nonzeroIdx = Rij ~= 0;
-                obj.Agraph(nonzeroIdx) = 1./Agraph(nonzeroIdx);  
+                nonzeroIdx = Rij_mat ~= 0;
+                obj.Agraph(nonzeroIdx) = 1./obj.Agraph(nonzeroIdx);  
                 obj.graph = digraph(obj.Agraph);
+                obj = obj.setSelectionMatrices(obj);
+                obj.L_tilde = diag(sum(obj.Agraph))-obj.Agraph;
             end
         end
         
         % initialize electrical parameters
-        function obj = initElecParam(obj, idx_DGU, Vin, Vr, Il, Ri, Ct, Li)
+        function obj = initElecParam(obj, idx_DGU, Vin, Vr, Il, Ri, Ct, Li, ...
+                                     Vmax, Vmin, Imax, Imin)
             obj.Vin(idx_DGU) = Vin;
             obj.Vr(idx_DGU) = Vr;
             obj.Il(idx_DGU) = Il;
             obj.Ri(idx_DGU) = Ri;
             obj.Ct(idx_DGU) = Ct;
             obj.Li(idx_DGU) = Li;
+            obj.Vmax(idx_DGU) = Vmax;
+            obj.Vmin(idx_DGU) = Vmin;
+            obj.Imax(idx_DGU) = Imax;
+            obj.Imin(idx_DGU) = Imin;
         end
+        
+         
         
         % initialize DGU dynamics
         function obj = initDynamics(obj,idx_DGU)
             for i = idx_DGU  % index of DGU (can be multiple) to get dynamics
-                if isempty(obj.Ct) || isempty(obj.Vr)
-                    error("Electrical parameters not correctly initialized or missing")
-                end
+%                 if isempty(obj.Ct) || isempty(obj.Vr)
+%                     error("Electrical parameters not correctly initialized or missing")
+%                 end
                 Ac = [0, 1/obj.Ct(i); -1/obj.Li(i), -obj.Ri(i)/obj.Li(i)];
                 Bc = [0; obj.Vin(i)/obj.Li(i)];
                 Fc = [1/obj.Ct(i); 0];
@@ -86,53 +100,22 @@ classdef DGU_network
             obj.A_Ni = change_system_representation(obj.Ai, obj.Fi, obj.Ci, obj.Agraph);
             % function compute references here
             [obj.di_ref, obj.Iti_ref] = compute_ref(obj.nb_subsystems, obj.Agraph,...
-                                                    obj.Vr, obj.Il, obj.Rij, obj.Ri, obj.Vin)    
+                                                    obj.Vr, obj.Il, obj.Rij_mat, obj.Ri, obj.Vin);    
             % Compute constraints
-            set_constraints()
+            obj = obj.setConstraints(obj);
+            
         end
-        
-        % change representation to neighbor state matrix
-        function obj = change_system_representation(obj)   
-            %A_Ni = cell(1,M);
-            if length(obj.Ai) ~= obj.nb_subsystems
-                error("Provide all system dynamics before changing to A_Ni");
-            end
-            for i=1:obj.nb_subsystems
-                out_neighbors = sort([i;successors(obj.graph, i)]);
-                Acell = cell(1,length(out_neighbors));
-                for j=1:length(out_neighbors)
-                    if isequal(out_neighbors(j), i)
-                        Acell{j} = obj.Ai{i} - obj.Fi{i}*sum(obj.Agraph(i,:))*obj.Ci{i};
-                    else
-                        Acell{j} = obj.Fi{i}*obj.Agraph(i, out_neighbors(j))*...
-                                             obj.Ci{out_neighbors(j)};
-                    end
-                end
-                obj.A_Ni{i} = cell2mat(Acell);  
-            end
-        end
-         
-        function obj = compute_ref(obj)
-            %di_ref = zeros(1,M); Iti_ref = zeros(1,M);
-            G = digraph(obj.Agraph);
-            for i= 1:obj.nb_subsystems
-                out_neighbors = sort(successors(G, i));
-                Vi = repmat(obj.Vr(i), size(obj.Vr(out_neighbors)));
-                sum_over_Ni = sum((Vi - obj.Vr(out_neighbors))./...
-                              obj.Rij_mat(i,out_neighbors));
-                obj.di_ref(i) = (obj.Vr(i)+ obj.Il(i)*obj.Ri(i))/obj.Vin(i) + ...
-                                 obj.Ri(i)/obj.Vin(i)*sum_over_Ni;
-                obj.Iti_ref(i) = (obj.di_ref(i)*obj.Vin(i) - obj.Vr(i))/obj.Ri(i);
-            end     
-        end
-        
-        function obj = setConstraints(obj)
+    end
+    
+       methods (Static)
+       function obj = setConstraints(obj)
               for i= 1:obj.nb_subsystems
                 obj.Xref{i} = [obj.Vr(i); obj.Iti_ref(i)-obj.Il(i)];
-                obj.Uref{i} = obj.di_ref{i};
-                obj.Gx_i{i}=  [eye(2); -eye(2)];
+                obj.Uref{i} = obj.di_ref(i);
+                obj.Gx_i{i}=  [eye(obj.ni); -eye(obj.ni)];
                 obj.Gu_i{i} = [1;-1];
-                obj.fx_i{i} = [52; 10-obj.Il(i); -49; 0+obj.Il(i)];
+                obj.fx_i{i} = [obj.Vmax(i); obj.Imax(i)-obj.Il(i);...
+                                -obj.Vmin(i); -obj.Imin(i)+obj.Il(i)];
                 obj.fu_i{i} = [1;0];
               end 
               for i= 1:obj.nb_subsystems
@@ -140,7 +123,31 @@ classdef DGU_network
                 obj.Gx_Ni{i} = blkdiag(obj.Gx_i{out_neighbors});
                 obj.fx_Ni{i} = vertcat(obj.fx_i{out_neighbors});
               end   
-        end
+       end
+        
+       function obj = setSelectionMatrices(obj)
+           state_i = eye(obj.ni);
+           N = obj.nb_subsystems*obj.ni;
+           for i = 1:obj.nb_subsystems
+                obj.U{i} = [zeros(obj.ni,obj.ni*(i-1)), state_i, zeros(obj.ni,N-obj.ni*i)];
+                out_neighbors = sort([i;successors(obj.graph, i)]); % neighbor states of i
+                obj.W{i} = zeros(obj.nb_subsystems);
+                % put a 1 in diagonal of neighbor state
+                for j=1:length(out_neighbors)
+                    obj.W{i}(out_neighbors(j),out_neighbors(j)) = 1;
+                end
+                obj.W{i}= kron(obj.W{i}, state_i); % each subsystem has size mi
+                obj.W{i}(all(obj.W{i}==0,2),:)=[]; % remove all zero rows
+            end
+            % Create Wij: extract state j from neighbor set of state i (j belongs to
+            % xNi)
+            for i = 1:obj.nb_subsystems
+                out_neighbors = sort([i;successors(obj.graph, i)]); % neighbor states of i
+                for j = 1:length(out_neighbors)
+                     obj.Wij{i}{out_neighbors(j)} = obj.U{out_neighbors(j)}*obj.W{i}';
+                end
+            end
+       end
         
     end  % end methods
 end  % end class
@@ -153,8 +160,6 @@ function A_Ni = change_system_representation(Ai,Fi,Ci,Agraph)
             for i=1:M
                 out_neighbors = sort([i;successors(G, i)]);
                 Acell = cell(1,length(out_neighbors));
-                %Acell{i} = Ai{i} - Fi{i}*sum(Agraph(i,:))*Ci{i};
-                %out_neighbors(i) = [];
                 for j=1:length(out_neighbors)
                     if isequal(out_neighbors(j), i)
                         Acell{j} = Ai{i} - Fi{i}*sum(Agraph(i,:))*Ci{i};
@@ -181,5 +186,4 @@ function [di_ref, Iti_ref] = compute_ref(M, Agraph, Vr, Il, Rij, R, Vin)
                 di_ref(i) = (Vr(i)+ Il(i)*R(i))/Vin(i) + R(i)/Vin(i)*sum_over_Ni;
                 Iti_ref(i) = (di_ref(i)*Vin(i) - Vr(i))/R(i);
             end     
-        end
-        
+end
--- mainPnP.m
+++ mainPnP.m
@@ -1,8 +1,41 @@
 %% Main File for PnP
 
 %% Initial DGU Network
-nb_subsystems = 6;
+
+filename = 'config_DGU_1.txt';
+[nb_subsystems, Vin,R,L,C, Vmax, Vmin, Imax, Imin] = importData(filename);
+
 Rij_mat = zeros(nb_subsystems);
 Rij_mat(1,2) = 1.75; Rij_mat(2,3) = 3.5; Rij_mat(2,4) = 1.75; 
-Rij_mat(3,5) = 2.8; Rij_mat(3,6) = 1.75;
+Rij_mat(3,5) = 2.8;
 Rij_mat = Rij_mat + tril(Rij_mat',1);
+
+dguNet = DGU_network(nb_subsystems, Rij_mat);
+% references
+Vr = linspace(49.95, 50.2, nb_subsystems);
+Il = linspace(2.5, 7.5, nb_subsystems);
+% set Electrical parameters
+for i=1:nb_subsystems
+    dguNet = dguNet.initElecParam(i,Vin(i), Vr(i), Il(i), R(i), C(i), L(i), ...
+                                  Vmax(i), Vmin(i), Imax(i), Imin(i));
+end
+dguNet = dguNet.initDynamics([1:1:nb_subsystems]);
+
+
+
+
+
+function [subsystems, Vin, R, L, C, Vmax, Vmin, Imax, Imin] = importData(filename)
+    delimiterIn = ';';
+    headerlinesIn= 1;
+    dataDGU = importdata(filename, delimiterIn, headerlinesIn);
+    subsystems = dataDGU.data(1,1);
+    Vin = dataDGU.data(:,2);
+    R = dataDGU.data(:,3);
+    L = dataDGU.data(:,4);
+    C = dataDGU.data(:,5);
+    Vmax = dataDGU.data(:,6);
+    Vmin = dataDGU.data(:,7);
+    Imax = dataDGU.data(:,8);
+    Imin = dataDGU.data(:,9);
+end
