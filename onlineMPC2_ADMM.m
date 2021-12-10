3e140ae3d2a7a0e8c1c4a56e3bb66fa512d48190
Nicolashoi <nhoischen@student.ethz.ch>
2021-12-10 11:06:50 +0100

    update admm

--- onlineMPC2_ADMM.m
+++ onlineMPC2_ADMM.m
@@ -42,16 +42,16 @@ function mpc_optimizer = init_optimizer(Ki, Q_Ni, Ri, Pi, N, param)
     Tk = 0; k = 2; l=0;
      for i=param.activeDGU
             neighbors_i = sort([i;neighbors(param.NetGraph, i)]);
-            z_Ni{i,1}.x_Ni = zeros(length(neighbors_i),1);
+            z_Ni{i,1}.x_Ni = zeros(length(neighbors_i),N);
             z_Ni{i,1}.x_eNi = zeros(length(neighbors_i),1);
-            z_Ni{i,1}.alpha_Ni = zeros(length(neighbors_i),1);
-            z_Ni{i,1}.c_Ni = 0; %%% TO CHECK SIZES
-            
-            y_Ni{i,1}.x_Ni = zeros(length(neighbors_i),1);
+            %z_Ni{i,1}.alpha_Ni = zeros(length(neighbors_i),1);
+           % z_Ni{i,1}.c_Ni = 0; %%% TO CHECK SIZES
+
+            y_Ni{i,1}.x_Ni = zeros(length(neighbors_i),N);
             y_Ni{i,1}.x_eNi = zeros(length(neighbors_i),1);
-            y_Ni{i,1}.alpha_Ni = zeros(length(neighbors_i),1);
-            y_Ni{i,1}.c_Ni = 0; %%% TO CHECK SIZES
-            
+            %y_Ni{i,1}.alpha_Ni = zeros(length(neighbors_i),1);
+            %y_Ni{i,1}.c_Ni = 0; %%% TO CHECK SIZES
+
     end
     while(Tk < TMAX)
     %% Constraints: Outer loop over subsystems, inner loop over Horizon
@@ -70,7 +70,7 @@ function mpc_optimizer = init_optimizer(Ki, Q_Ni, Ri, Pi, N, param)
             % obtain sorted list of neighbors of system i
             neighbors_i = sort([i;neighbors(param.NetGraph, i)]);
 
-            lambda{i} = sdpvar(n_Ni,1, 'full');
+            %lambda{i} = sdpvar(n_Ni,1, 'full');
             % constraint for X_eNi =  concat of neighbor x_ei
     %         constraints_i = [constraints_i, X_eNi == ...
     %                                     reshape(Xe(:,neighbors_i),[],1)];
@@ -102,12 +102,15 @@ function mpc_optimizer = init_optimizer(Ki, Q_Ni, Ri, Pi, N, param)
                 % Objective
                 objective = objective + ...
                             (X_Ni(:,n)-X_eNi)'*Q_Ni{i}*(X_Ni(:,n)-X_eNi)+...
-                            (Ui(:,n)-Uei)'*Ri{i}*(Ui(:,n)-Uei);                    
+                            (Ui(:,n)-Uei)'*Ri{i}*(Ui(:,n)-Uei) + ...
+                            y_Ni{i,k}.x_Ni(:,n)*(w_Ni{i,k}.x_Ni(:,n)-z_Ni{i,k}.x_Ni{:,n)...
+                            +;                    
             end
             %% Terminal cost
-            objective = objective + ... 
-                        (Xi{end}(:,i)-Xe(:,i))'*Pi{i}*(Xi{end}(:,i)-Xe(:,i))+...
-                        (Xe(:,i) - param.Xref{i})'*Si*(Xe(:,i) - param.Xref{i});
+            objective = objective + y_Ni{i,k}.x_Ni*w_Ni{i,k}
+%             objective = objective + ... 
+%                         (Xi{end}(:,i)-Xe(:,i))'*Pi{i}*(Xi{end}(:,i)-Xe(:,i))+...
+%                         (Xe(:,i) - param.Xref{i})'*Si*(Xe(:,i) - param.Xref{i});
 
             %%  Terminal Set condition
             constraints_i = [constraints_i, (Xi{end}(:,i)-ci(i))'*Pi{i}*(Xi{end}(:,i)-ci(i))...
@@ -120,29 +123,29 @@ function mpc_optimizer = init_optimizer(Ki, Q_Ni, Ri, Pi, N, param)
             wNi{i,k}.x_Ni = value(X_Ni);
             wNi{i,k}.x_eNi = value(X_eNi);
             wNi{i,k}.alpha_Ni = value(X_eNi);
-            wNi{i,k}.cNi = value(cNi);
+            %wNi{i,k}.cNi = value(cNi);
             vi{i,k}.ui = value(Ui);
             vi{i,k}.uei = value(Uei);
             vi{i,k}.di = value(di);
-            vi{i,k}.lambda_ij = value(lambda_ij);
+            %vi{i,k}.lambda_ij = value(lambda_ij);
             wi{i,k,i}.xi = value(Xi);
             wi{i,k,i}.xei = value(Xei);
-            wi{i,k,i}.alpha_i = value(alpha_i); % ith value computed by system i
-            wi{i,k,i}.ci = value(ci);
+            %wi{i,k,i}.alpha_i = value(alpha_i); % ith value computed by system i
+            %wi{i,k,i}.ci = value(ci);
             
         end    
         for i=param.activeDGU
             z{i,k}.xi = mean(horzcat(wi{i,k,:}.xi),2);
             z{i,k}.xei = mean(horzcat(wi{i,k,:}.xei),2); 
-            z{i,k}.alpha_i = mean(horzcat(wi{i,k,:}.alpha_i),2); 
-            z{i,k}.ci = mean(horzcat(wi{i,k,:}.ci),2); 
+            %z{i,k}.alpha_i = mean(horzcat(wi{i,k,:}.alpha_i),2); 
+            %z{i,k}.ci = mean(horzcat(wi{i,k,:}.ci),2); 
         end
         for i=param.activeDGU
             neighbors_i = sort([i;neighbors(param.NetGraph, i)]);
             z_Ni{i,k}.x_Ni = vertcat(z{neighbors_i,k}.xi ,[],1);
             z_Ni{i,k}.x_eNi = vertcat(z{neighbors_i,k}.xei ,[],1);
-            z_Ni{i,k}.alpha_Ni = vertcat(z{neighbors_i,k}.alpha_i ,[],1);
-            z_Ni{i,k}.c_Ni = vertcat(z{neighbors_i,k}.ci ,[],1);
+            %z_Ni{i,k}.alpha_Ni = vertcat(z{neighbors_i,k}.alpha_i ,[],1);
+            %z_Ni{i,k}.c_Ni = vertcat(z{neighbors_i,k}.ci ,[],1);
             
         end
     end
@@ -156,3 +159,25 @@ function mpc_optimizer = init_optimizer(Ki, Q_Ni, Ri, Pi, N, param)
     solutions_out = Ui{1}; % get U0 for each subsystem, size nu x M
     mpc_optimizer = optimizer(constraints_i,objective,ops,parameters_in,solutions_out);
 end
+
+
+function [w_Ni_new, vi_new] = lagrangian(N, p, constraints_i, objective_i, w_Ni, vi, z_Ni, y_Ni)
+    fn = fieldname(w_Ni);
+    for ii = 1:numel(fn)
+        objective = objective + y_Ni.(fn{ii}).*(w_Ni.(fn{ii}) - z_Ni.(fn{ii})) + ...
+                    p/2*(w_Ni.(fn{ii}) - z_Ni.(fn{ii})).^2;
+    end
+    ops = sdpsettings('solver', 'MOSEK', 'verbose',1); %options
+    diagnostics = optimize(constraints, objective, ops);
+    if diagnostics.problem == 1
+       fprintf("MOSEK solver thinks it is infeasible");
+    end
+    for ii = 1:numel(fn)
+        w_Ni_new.(fn{ii}) = value(w_Ni.(fn{ii}));
+    end
+    fnv = fieldname(vi);
+    for iii = 1:numel(fnv)
+        vi_new.(fnv{iii}) = value(vi.(fnv{iii}));
+    end
+end
+
